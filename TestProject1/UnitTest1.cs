060539055c5ae92ae708037880ee7a29c49dfeb4
215557299 <38215557299@mby.co.il>
2025-03-04 13:45:11 +0200

    04/03/25

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,32 +1,32 @@
-using System.Reflection.Metadata;
-using Entites;
-using Moq;
-using Moq.EntityFrameworkCore;
-using Resources;
+//using System.Reflection.Metadata;
+//using Entites;
+//using Moq;
+//using Moq.EntityFrameworkCore;
+//using Resources;
 
 
-namespace TestProject1
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public async Task GetUser_ValidCredentials_ReturnsUser()
-        {
-            //Arrange
-            var user = new User { FirstName = "Rut", UserName = "test@test.test", Password = ")(*fkjl123" };
+//namespace TestProject1
+//{
+//    public class UnitTest1
+//    {
+//        [Fact]
+//        public async Task GetUser_ValidCredentials_ReturnsUser()
+//        {
+//            //Arrange
+//            var user = new User { FirstName = "Rut", UserName = "test@test.test", Password = ")(*fkjl123" };
 
-            var mockContext = new Mock<ApiManagerContext>();
-            var users = new List<User>() { user };
-            mockContext.Setup(x => x.Users).ReturnsDbSet(users);
+//            var mockContext = new Mock<ApiManagerContext>();
+//            var users = new List<User>() { user };
+//            mockContext.Setup(x => x.Users).ReturnsDbSet(users);
 
-            var userRepository = new UserRepository(mockContext.Object);
+//            var userRepository = new UserRepository(mockContext.Object);
 
-            //Act 
-            var result = await userRepository.PostLogIn(user.UserName, user.Password);
+//            //Act 
+//            var result = await userRepository.PostLogIn(user.UserName, user.Password);
 
-            //Assert
-            Assert.Equal(user, result);
-        }
-    }
+//            //Assert
+//            Assert.Equal(user, result);
+//        }
+//    }
 
-}
+//}
--- TestProject1/UserRepositoryUnitTesting.cs
+++ TestProject1/UserRepositoryUnitTesting.cs
@@ -114,3 +114,4 @@ namespace TestProject1
     //    }
     //}
 }
+    }
